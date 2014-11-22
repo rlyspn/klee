54c4b6ff6f68b4fedc7cab77188d25c63035c98a
David Williams-King <dwk@cs.columbia.edu>
2014-11-22 14:49:57 -0500

    Merge?

--- examples/max/max.c
+++ examples/max/max.c
@@ -13,16 +13,16 @@ int main() {
 
     int m0 = max(a1, a2, a3, a4);
 
-    //klee_make_symbolic(&a1, sizeof(a1), "a1");
+    klee_make_symbolic(&a1, sizeof(a1), "a1");
     int m1 = max(a1, a2, a3, a4);
 
-    //klee_make_symbolic(&a2, sizeof(a2), "a2");
+    klee_make_symbolic(&a2, sizeof(a2), "a2");
     int m2 = max(a1, a2, a3, a4);
 
-    //klee_make_symbolic(&a3, sizeof(a3), "a3");
+    klee_make_symbolic(&a3, sizeof(a3), "a3");
     int m3 = max(a1, a2, a3, a4);
 
-    //klee_make_symbolic(&a4, sizeof(a4), "a4");
+    klee_make_symbolic(&a4, sizeof(a4), "a4");
     int m4 = max(a1, a2, a3, a4);
 
     //printf("%d %d %d %d %d\n", m0, m1, m2, m3, m4);
