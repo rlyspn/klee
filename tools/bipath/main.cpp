36f159465e755d5a2ab53b6b94798a82262d1b21
Riley Spahn <rileyspahn@gmail.com>
2014-11-23 20:49:15 -0500

    -added input and output directory. -added output writing.

--- tools/bipath/main.cpp
+++ tools/bipath/main.cpp
@@ -4,6 +4,7 @@
 
 #include "expr/Parser.h"
 
+#include "klee/Constraints.h"
 #include "klee/ExprBuilder.h"
 #include "klee/util/ExprPPrinter.h"
 
@@ -12,6 +13,9 @@
 #include "llvm/Support/raw_ostream.h"
 
 #include <dirent.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/stat.h>
 #include <sys/types.h>
 #include <vector>
 
@@ -46,7 +50,7 @@ static bool getPCFiles(const std::string inputDir,
         std::string file_name(ent->d_name);
         std::size_t index = file_name.find(".pc");
         if (index != std::string::npos && index == file_name.size() - 3) {
-            pcsFiles->push_back(inputDir + file_name);
+            pcsFiles->push_back(file_name);
         }
     }
     closedir(dir);
@@ -117,8 +121,74 @@ static bool rewriteConstraints(const std::string inputFile,
     return true;
 }
 
-static bool writeNewPCFile(std::string newFile, std::vector<Decl*> *decls)
+static bool writeArrayDecl(raw_fd_ostream *rfo, ArrayDecl *ad)
 {
+    *rfo << "array " << ad->Root->name << "[" << ad->Root->size << "]" << " : "
+        << 'w' << ad->Domain << " -> " << 'w' << ad->Range << " = ";
+    if (ad->Root->isSymbolicArray()) {
+        *rfo << "symbolic\n";
+    } else {
+        for (unsigned int i = 0, e = ad->Root->size; i != e; i++) {
+            if (i) {
+                *rfo << " ";
+            }
+            *rfo << ad->Root->constantValues[i];
+        }
+        *rfo << "]\n";
+    }
+    return true;
+}
+
+static bool writeQueryDecl(raw_fd_ostream *rfo, QueryCommand *qc)
+{
+  const ExprHandle *ValuesBegin = 0, *ValuesEnd = 0;
+  const Array * const* ObjectsBegin = 0, * const* ObjectsEnd = 0;
+  if (!qc->Values.empty()) {
+    ValuesBegin = &(qc->Values[0]);
+    ValuesEnd = ValuesBegin + qc->Values.size();
+  }
+  if (!qc->Objects.empty()) {
+    ObjectsBegin = &(qc->Objects[0]);
+    ObjectsEnd = ObjectsBegin + qc->Objects.size();
+  }
+  ExprPPrinter::printQuery(*rfo, ConstraintManager(qc->Constraints),
+                           qc->Query, ValuesBegin, ValuesEnd,
+                           ObjectsBegin, ObjectsEnd,
+                           false);
+  return true;
+}
+
+static bool writeNewPCFile(std::string outFile, std::vector<Decl*> *decls)
+{
+    raw_fd_ostream *rfo;
+    std::string rfo_err;
+    rfo = new raw_fd_ostream(outFile.c_str(), rfo_err, O_TRUNC);
+    if (!rfo_err.empty()) {
+        delete rfo;
+        return false;
+    }
+    for (unsigned int i = 0; i < decls->size(); i++) {
+        Decl *decl = (*decls)[i];
+
+        if (decl->getKind() == Decl::QueryCommandDeclKind) {
+            writeQueryDecl(rfo, static_cast<QueryCommand*>(decl));
+        } else if (decl->getKind() == Decl::ArrayDeclKind) {
+            writeArrayDecl(rfo, static_cast<ArrayDecl*>(decl));
+        }
+    }
+
+    delete rfo;
+
+    return true;
+}
+
+
+
+static bool prepareOutputDir(std::string outputDir) {
+    if (mkdir(outputDir.c_str(), 0777) == -1) {
+        return errno == EEXIST;
+    }
+    return true;
 }
 
 int main(int argc, char **argv) {
@@ -127,11 +197,12 @@ int main(int argc, char **argv) {
      * Start CL Parsing
      * TODO: Use llvm ParseCommandLineOptions
      ***************************************/
-    if (argc != 2) {
-        llvm::errs() << "Expected: bipath <input dir>\n";
+    if (argc != 3) {
+        llvm::errs() << "Expected: bipath <input dir> <output dir>\n";
         return -1;
     }
     std::string inputDir(argv[1]);
+    std::string outputDir(argv[2]);
     std::vector<std::string> pcsFiles;
     
     if (!getPCFiles(inputDir, &pcsFiles)) {
@@ -139,12 +210,16 @@ int main(int argc, char **argv) {
         return -1;
     }
 
-
     std::vector<std::string>::iterator iter;
+    prepareOutputDir(outputDir);
     for (unsigned int i = 0; i < pcsFiles.size(); i++) {
         std::vector<Decl*> newDecls;
-        rewriteConstraints(pcsFiles[i], &newDecls);
-        writeNewPCFile(pcsFiles[i] + ".new", &newDecls);
+        /* TODO: Safely do this. */
+        std::string inFile = inputDir + pcsFiles[i];
+        std::string outFile = outputDir + pcsFiles[i];
+
+        rewriteConstraints(inFile, &newDecls);
+        writeNewPCFile(outFile, &newDecls);
     }
 
     return 1;
